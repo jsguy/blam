92757be60b204253998c7d8e5e4a845e17b5284e
jsguy <mikkel@pointful.com>
2011-01-04 11:25:13 +1100

    Re-added tmp, and made script tmp aware

--- blam.sh
+++ blam.sh
@@ -2,6 +2,26 @@
 
 #
 #	blam.sh - Beautify Lint and Minify JS files
+#   Copyright (c) 2011 - Mikkel Bergmann - http://jsguy.com
+#   
+#
+#	Licence
+#	-------
+#	
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Lesser General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Lesser General Public License for more details.
+#
+#   You should have received a copy of the GNU Lesser General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#   See lgpl.txt for licence details
 #
 
 # Get parameters, defaults below
@@ -72,6 +92,10 @@ if [[ ! -f "$inputfile" ]]; then
 		echo "File not found: $inputfile"
 	fi
 	exit 1
+fi
+        
+if [ ! -d ./tmp ]; then
+    mkdir ./tmp
 fi
 
 # Internal variables
